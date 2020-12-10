0ded7659b1dc94235d79eec7e7f79f55366800fc
YiminZhao <doctormin@sjtu.edu.cn>
2020-12-10 19:30:47 +0800

    downloader: finish the top interface

--- utils/distributer.py
+++ utils/distributer.py
@@ -4,4 +4,11 @@ class my_distributer:
     def __init__(self) -> None:
         pass
 
+    def download_interval_for_threads(self, left_point, right_point, thread_number) -> list[list]:
+        '''
+        此函数将[left_point, right_point]区间分为thread_number块，并返回list[thread_number][2]
+        '''
+        # TODO:
+
+
      
--- utils/downloader.py
+++ utils/downloader.py
@@ -1,50 +1,53 @@
-import threading 
+import threading
 from colorama import Fore, Style  # 骚气的输出，用来强调warning信息
 from utils.requests import my_requests
-from utils.file_tools import file_tools, my_file_tools
-
+from utils.file_tools import my_file_tools
+from utils.distributer import my_distributer
 
 
 class my_downloader:
     def __init__(self) -> None:
-        self.url          : str  = None
-        self.proxies      : dict = None
-        self.tmp_dir      : str  = None  # 临时文件夹 (存放着多线程下载得到的file segments)
-        self.target_dir   : str  = None  # 目标文件夹 (合并后的下载文件存放目录)
-        self.file_name    : str  = None  # 目标文件名 (合并后的下载文件名)
-        self.thread_number: int  = None  # 多线程下载中的线程个数，在server的config文件中声明
+        self.url: str = None
+        self.proxies: dict = None
+        self.tmp_dir: str = None  # 临时文件夹 (存放着多线程下载得到的file segments)
+        self.target_dir: str = None  # 目标文件夹 (合并后的下载文件存放目录)
+        self.file_name: str = None  # 目标文件名 (合并后的下载文件名)
+        self.thread_number: int = None  # 多线程下载中的线程个数，在server的config文件中声明
 
-    def _multi_thread_download(self, download_list) -> None:
+    def _multi_thread_download(self, download_interval_list) -> None:
         '''
         工具函数（不对外暴露）
-        负责多线程下载, 第i个线程下载的文件为`${tmp_dir}/segment_by_thread_${i}`
+        负责多线程下载, 第i个线程下载的文件为`${tmp_dir}segment_by_thread_${i}`
         '''
         for thread_id in range(self.thread_number):
-            # calling Downloader.download_range() for each thread
             t = threading.Thread(target=my_requests().partial_request,
-                kwargs={
+                                 kwargs={
                 'url': self.url,
                 'proxies': self.proxies,
-                'target_dir': self.tmp_dir + 'segment_by_thread_' + str(thread_id),
-                'left_point': None, # TODO
-                'left_point': None, # TODO
-                })
+                # file segments的存储路径
+                'file_path': self.tmp_dir + 'segment_by_thread_' + str(thread_id),
+                # 第${thread_id}个线程的下载开始比特数
+                'left_point': download_interval_list[thread_id][0],
+                # 第${thread_id}个线程的下载结束比特数
+                'right_point': download_interval_list[thread_id][1]
+            })
             t.setDaemon(True)
-            t.start()   
+            t.start()
         main_thread = threading.current_thread()
         for each in threading.enumerate():
             if each is main_thread:
                 continue
-            each.join()  
+            each.join()
 
     def _merge_file_segments(self) -> None:
         '''
         工具函数（不对外暴露）
-        负责合并多线程下载得到的file segments, 并将文件存为${target_dir}/${file_name}
+        负责合并多线程下载得到的file segments, 并将文件存为${target_dir}${file_name}
         '''
-        with open(self.target_dir + '/' + self.file_name, mode='wb') as target_file:
-            file_segments_list = [self.tmp_dir + 'segment_by_thread_' + str(thread_id) for thread_id in range(self.thread_number)]
-            my_file_tools().append_file(file_segments_list, target_file)   
+        with open(self.target_dir + self.file_name, mode='wb') as target_file:
+            file_segments_list = [self.tmp_dir + 'segment_by_thread_' +
+                                  str(thread_id) for thread_id in range(self.thread_number)]
+            my_file_tools().append_file(file_segments_list, target_file)
             for file_segment in file_segments_list:
                 my_file_tools().delete_file(file_segment)
 
@@ -53,16 +56,16 @@ class my_downloader:
         唯一的对外接口，由distributed-server调用，负责下载文件片段，
         对于该片段，在支持多线程下载时采用多线程下载，不支持时采用单线程下载
         '''
-        self.url        = url
-        self.proxies    = proxies
-        self.tmp_dir    = tmp_dir
+        self.url = url
+        self.proxies = proxies
+        self.tmp_dir = tmp_dir
         self.target_dir = target_dir
 
         if my_requests().partial_supported(url, proxies) == True:
             # 多线程下载
-            # TODO: download_list = ?
-            download_list = []
-            self._multi_thread_download(download_list)
+            download_interval_list = my_distributer().download_interval_for_threads(
+                left_point, right_point, self.thread_number)
+            self._multi_thread_download(download_interval_list)
             self._merge_file_segments()
         else:
             # 单线程下载
@@ -71,5 +74,5 @@ class my_downloader:
             my_requests.partial_request(url=url,
                                         left_point=left_point,
                                         right_point=right_point,
-                                        file_path=target_dir,
+                                        file_path=self.target_dir + self.file_name,  # 目标文件存储位置
                                         proxies=proxies)
