aa84f3eaa7f6cd56f65352cbbd1798868b75cba2
YiminZhao <doctormin@sjtu.edu.cn>
2020-12-10 22:04:59 +0800

    requests: finish partial_request(...) (test passed)

--- utils/requests.py
+++ utils/requests.py
@@ -22,12 +22,19 @@ class my_requests:
 
 
     def partial_request(self, url: str, left_point: int, right_point: int, file_path: str, proxies: dict = None, timeout=5) -> None:
-        resp = requests.Session.request(method="GET",
+        '''
+        此函数将被多线程执行，负责将${url}的[${left_point}, ${right_point}]比特下载为${file_path}
+        '''
+        resp = requests.Session().request(method="GET",
                                         url=url,
                                         proxies=proxies,
                                         timeout=timeout,
                                         headers={
-                                            'Range': 'bytes=%d-%d' % (left_point, right_point)}
+                                            'Range': 'bytes=%d-%d' % (left_point, right_point)},
+                                        stream=True # 分块写入，防止大文件下载导致内存溢出
                                         )
-        # TODO: downloading process to be finished
+        with open(file_path, mode='wb') as wf:
+            for chunk in resp.iter_content(chunk_size=5120):
+                if chunk:
+                    wf.write(chunk)
         resp.close()
