ff5a5f5647e602202e21d1582f4052d1e471c67a
Zhicun Chen <703388573@qq.com>
2020-12-11 23:37:34 +0800

    work on daemon

--- client/worker/client_daemon.py
+++ client/worker/client_daemon.py
@@ -4,7 +4,7 @@ class client_daemon:
         self.server_set = set()
 
     @staticmethod
-    def fetch_server_list(track_address: str, client_tracker_bind_port: str) -> None:
+    def fetch_server_list(track_address: tuple, client_tracker_bind_port: str) -> None:
         # TODO: add server address to self.server_set
         pass
 
