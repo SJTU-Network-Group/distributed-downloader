378428cee8fea1d022e08d9d95bbf275e234bc5b
YiminZhao <doctormin@sjtu.edu.cn>
2020-12-12 21:11:55 +0800

    manager: draft of daemon finished

--- manager/worker/manager_daemon.py
+++ manager/worker/manager_daemon.py
@@ -6,32 +6,37 @@ from colorama import Fore, Style
 from utils.requests import MyRequests
 from utils.distributor import MyDistributor
 from utils.file_tools import MyFileTools
-from client.worker.client_thread import ClientThread
+from manager.worker.manager_thread import ManagerThread
+
 
 class ManagerDaemon:
     def __init__(self,
                  manager_addr_ipv4: str,
-                 to_server_port: int,
-                 to_client_port: int
+                 listen_port: int        # manager
                  ) -> None:
         self.manager_addr_ipv4 = manager_addr_ipv4
-        self.to_server_port = to_server_port
-        self.to_client_port = to_client_port
-        self.active_server_addr_ipv4 = []
-        # IP list for active servers
-        self.to_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.to_server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.to_server_socket.bind((self.manager_addr_ipv4, self.to_server_port))
-        self.to_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.to_client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # dubious
-        self.to_client_socket.bind((self.manager_addr_ipv4, self.to_client_port))
+        self.listen_port = listen_port
 
-    def listen_for_client(self) -> None:
-        self.to_client_socket.listen(5)  # dubious
+        # 初始化监听的socket
+        self.listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.listen_socket.bind((self.manager_addr_ipv4, self.listen_port))
+
+        self.mutex = threading.Lock()
+        self.server_list = []  # 格式为: [(server的ipv4地址: str, server的端口号: int),...]
+
+    def listen(self) -> None:
+        self.listen_socket.listen(5)
         print(
-            f"listening on {self.manager_addr_ipv4}:{str(self.to_client_port)}...")
+            f"listening on {self.manager_addr_ipv4}:{str(self.listen_port)}...")
         while True:
-            conn_socket, client_addr_tuple = self.to_client_socket.accept()
+            conn_socket, conn_addr_tuple = self.listen_socket.accept()
             print(Fore.GREEN, "\nsucceed -> ", Style.RESET_ALL,
-                  f"connected to client: {client_addr_tuple[0]}:{str(client_addr_tuple[1])}")
-
+                  f"connected to : {conn_addr_tuple[0]}:{str(conn_addr_tuple[1])}")
+            _manager_thread = ManagerThread(
+                server_list=self.server_list,
+                conn_socket=conn_socket,
+                conn_addr_tuple=conn_addr_tuple,
+                mutex=self.mutex
+            )
+            _manager_thread.start()
