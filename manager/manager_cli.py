81ac27f37c91c98e4f7b41b571c31f2b91f6f76e
Zhicun Chen <703388573@qq.com>
2020-12-12 21:42:42 +0800

    finish client and server cli.

--- manager/manager_cli.py
+++ manager/manager_cli.py
@@ -2,7 +2,18 @@ import yaml
 import sys
 import os
 from utils.file_tools import MyFileTools
-from client.worker.client_daemon import ClientDaemon
+from manager.worker.manager_daemon import ManagerDaemon
 from utils.requests import MyRequests
 from colorama import Fore, Style
 import socket
+
+
+if __name__ == "__main__":
+    with open('./config/manager_config.yml', 'r') as f:
+        config = yaml.load(f, yaml.FullLoader)
+    manager_addr_ipv4 = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2]
+                                     if not ip.startswith("127.")][:1],
+                                    [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close())
+                                      for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
+    manager = ManagerDaemon(manager_addr_ipv4=manager_addr_ipv4, listen_port=config['MANAGER_PORT'])
+    manager.listen()
